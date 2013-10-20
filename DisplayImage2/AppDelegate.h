e66d8da2b252d49559df1b341452472f2dfb1334
Francisco Surroca <fsurroca@francisco-surrocas-macbook-pro.local>
2013-10-20 14:44:50 -0400

    Initial Commit

---
+++ DisplayImage2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DisplayImage2
+//
+//  Created by Francisco Surroca on 10/20/13.
+//  Copyright (c) 2013 Francisco Surroca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DisplayImage2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DisplayImage2
+//
+//  Created by Francisco Surroca on 10/20/13.
+//  Copyright (c) 2013 Francisco Surroca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
